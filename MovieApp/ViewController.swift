20d20097fd294d6c89cb26553b7ce55f8b4fe55e
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-03-01 12:52:26 +0100

    Initial commit

---
+++ MovieApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Macbook on 1. 3. 2021..
+//
+
+import UIKit
+import Alamofire
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView : UITableView!
+    @IBOutlet weak var rowView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
